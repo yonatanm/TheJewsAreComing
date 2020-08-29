66c1d3e07743f3a137031f0a93a6535b4d450766
Yonatan Maman <yonatan@outbrain.com>
2020-08-29 12:08:30 +0300

    conditionaly adding GA

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -4,6 +4,51 @@ require("dotenv").config({
 
 const siteURL = process.env.SITE_URL
 
+const defaultPlugins = [
+  {
+    resolve: "gatsby-source-google-sheets",
+    options: {
+      spreadsheetId: "1YvCCiDAlwdPzyq5tkl1XMWm2LgSaC5Faz3_I4AiEssI",
+      worksheetTitle: "sheet1",
+      credentials: require("./secret.json"),
+    },
+  },
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-default`,
+      short_name: `starter`,
+      start_url: `/`,
+      background_color: `#663399`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    },
+  },
+];
+
+const myPlugins = defaultPlugins
+
+if (process.env.GOOGLE_ANALYTICS === "true") {
+  console.log('@@ injecting GA @@')
+  myPlugins.push({
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-176710736-1",
+    },
+  })
+}
+
 module.exports = {
   siteMetadata: {
     title: `היהודים באים`,
@@ -12,45 +57,5 @@ module.exports = {
     url: siteURL,
     defaultImage: `https://www.thejewsarecoming.tv/static/homepage.jpg?willitwork=1`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-176710736-1",
-      },
-    },
-    {
-      resolve: "gatsby-source-google-sheets",
-      options: {
-        spreadsheetId: "1YvCCiDAlwdPzyq5tkl1XMWm2LgSaC5Faz3_I4AiEssI",
-        worksheetTitle: "sheet1",
-        credentials: require("./secret.json"),
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins: myPlugins
 }
